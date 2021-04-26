1d9ed94db501c612515fb8cc686017e5601b9cd1
Enzo17101 <78074212+Enzo17101@users.noreply.github.com>
2021-04-26 18:42:20 +0200

    annuaire

---
+++ Maquette/annuaire/menu_clic.js
@@ -0,0 +1,21 @@
+
+function open_context_menu(user_id)
+{
+  document.getElementById("context_menu").className = "show_context_menu";
+}
+
+/*function show_menu()
+{
+  document.getElementById("menu_burger").className = "move_menu";
+}
+
+function close_menu()
+{
+  var p = document.getElementById("burger_cross");
+  p.onclick = close_burger;
+};
+
+function hide_menu()
+{
+  document.getElementById("menu_burger").className = "burger";
+}*/
