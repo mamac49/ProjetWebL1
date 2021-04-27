4d18060562d35db691b3b62c71610e1f787e7e75
Enzo17101 <78074212+Enzo17101@users.noreply.github.com>
2021-04-27 18:45:41 +0200

    menu clic gauche

--- Maquette/annuaire/menu_clic.js
+++ Maquette/annuaire/menu_clic.js
@@ -1,8 +1,43 @@
 
-function open_context_menu(user_id)
-{
-  document.getElementById("context_menu").className = "show_context_menu";
+Mouse_X = 0 ;
+Mouse_Y = 0 ;
+contextMenuOpened = false;
+function detect_clic(){
+  var contact = document.getElementById("contact0");
+  contact.onclick = open_context_menu;
+  var contact = document.getElementById("contact1");
+  contact.onclick = open_context_menu;
+  var contact = document.getElementById("contact2");
+  contact.onclick = open_context_menu;
+  
 }
+ 
+
+//on récupère la position du curseur en pixels
+function posCur(evt) {
+  evt = !evt ? event : evt
+  Mouse_X = evt.clientX; 
+  Mouse_Y = evt.clientY;
+  var scroll_x=document.body.scrollLeft || document.documentElement.scrollLeft;
+  var scroll_y=document.body.scrollTop || document.documentElement.scrollTop;
+  //valeur finale de la position du curseur
+  Mouse_X += scroll_x;
+  Mouse_Y += scroll_y;
+}
+
+typeof window.addEventListener == 'undefined' ? document.attachEvent("onmousemove",posCur) : document.addEventListener('mousemove',posCur,false);
+
+function open_context_menu(){ 
+  //on défini dans le css où s'ouvrira notre fenêtre
+  menu = document.getElementById("context_menu"); 
+  menu.style.top =  (-688 + Mouse_Y) + "px";
+  menu.style.left = (-10 + Mouse_X) + "px";
+  //puis on change la classe du menu, pour qu'il s'affiche
+  document.getElementById("context_menu").className = "show_context_menu";
+  
+}
+
+
 
 /*function show_menu()
 {
