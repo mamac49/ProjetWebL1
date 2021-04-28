bb39b9d886eacc7393baf1203fbee0ac5e272028
Mattéo Langlois <mamac49@free.fr>
2021-04-28 12:32:22 +0200

    add a file with a request to show how it works

--- ENT/testconnexion.php
+++ ENT/testconnexion.php
@@ -1,10 +1,30 @@
 <?php
-error_reporting(E_ALL);
-require ('demo.inc.php');
-debutHtml('Connexion à la base');
-echo '<p> démonstration à venir de connexion à une base </p>' ;
-finHtml();
+function dbConnect() {
+        $link = new mysqli('localhost', 'ENT', 'uWBs4M9kIX4PVa2o', 'ENT');
 
-$link = mysqli_connect('localhost', 'phpmyadmin', 'Jelly49400', 'ENT');
+        if (mysqli_connect_errno()) {
+                echo 'Erreur d accès à la base' . mysqli_connect_error();
+                exit;
+        }
+        echo 'accès réussi'."\n";
+        return $link;
+}
+
+$link = dbConnect();
+
+$sql = "SELECT * FROM users";
+
+if ($result = mysqli_query($link, $sql)) {
+        //echo mysqli_num_rows($result);
+        while ( $row = mysqli_fetch_assoc($result)) {
+                echo $row['prenom'] . "\n" . $row['nom'], '<br>';
+        }
+
+        mysqli_free_result($result);
+}
+
+mysqli_close($link);
+
+
 
 ?>
