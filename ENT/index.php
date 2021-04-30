57b2889c570c0656557767e5af47308fd7383ca2
Mattéo Langlois <mamac49@free.fr>
2021-04-30 13:35:10 +0200

    change the settings

--- ENT/index.php
+++ ENT/index.php
@@ -35,7 +35,7 @@ if ($_SESSION["Connected"] = "True") {
 </html>
 <?php
 } else {
-  header(location: "/Projetwebl1/ENT/auth/auth.php");
+  header('location: https://mlanglois.freeboxos.fr//Projetwebl1/ENT/auth/auth.php');
 }
 
 ?>
