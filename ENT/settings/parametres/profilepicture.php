82ffd983b56b0974c6c3ec88ea55095c76ce3187
Mattéo Langlois <mamac49@free.fr>
2021-05-01 10:03:34 +0200

    show the PP in settings

--- ENT/settings/parametres/profilepicture.php
+++ ENT/settings/parametres/profilepicture.php
@@ -16,7 +16,7 @@ $link = dbConnect();
 $sql = "SELECT * FROM `users` WHERE `mail`= ?";
 $stmt = mysqli_prepare($link, $sql);
 $stmt = mysqli_stmt_bind_param($stmt, 1, $_GET["id"]);
-$stmt = mysli_execute();
+$stmt = mysli_stmt_execute();
 
 $row = mysqli_fetch_assoc($stmt);
 
