cd96899134f1804cdfb970c8f1a4edc19d761e57
Enzo17101 <78074212+Enzo17101@users.noreply.github.com>
2021-05-01 21:41:44 +0200

    comment

--- ENT/base.php
+++ ENT/base.php
@@ -1,55 +1,48 @@
-<?php
-
-if ($_SESSION["Connected"] == true) {
-?>
-
-<body onload="move_menu_burger(); ">
-
-  <div class="site_container">
-    <nav class="burger" id="menu_burger">
-      <ul class="menuListe">
-        <li><a class="menu_cross" id="burger_cross" onclick="refresh();"><i class="fas fa-times icone"></i></a></li>
-        <li><a class="menu_link" href="/Projetwebl1/ENT/publications/cmultimedia/cmedia.php"><div class="texteBurger"><i class="fas fa-book icone"></i>Cahiers multimédias</div></a></li>
-        <li><hr class="hrBurger"></li>
-        <li><a class="menu_link" href="/Projetwebl1/ENT/publications/blog/blog.php"><div class="texteBurger"><i class="fas fa-book-open icone"></i>Blog</div></a></li>
-        <li><hr class="hrBurger"></li>
-        <li><a class="menu_link" href="/Projetwebl1/ENT/publications/files/files.php"><div class="texteBurger"><i class="fas fa-file-archive icone"></i>Archivage de fichier</div></a></li>
-        <li><hr class="hrBurger"></li>
-        <li><a class="menu_link" href="/Projetwebl1/ENT/settings/annuaire/annuaire.php"><div class="texteBurger"><i class="far fa-address-book icone"></i>Annuaire</div></a></li>
-        <li><hr class="hrBurger"></li>
-      </ul>
-      <ul class="menuAutre">
-        <?php
-          if ($_SESSION["Admin"] == True) {
-        ?>
-        <li><a class="menu_link" href="/Projetwebl1/ENT/settings/admin/UserCreation.php"><div class="texteBurger"><i class="fas fa-user-cog icone"></i>Administration</div></a></li>
-        <?php
-        }
-        ?>
-        <li><a class="menu_link" onclick="LoadCSS('/Projetwebl1/ENT/css/color1.css')"><i class="fas fa-moon icone"></i>Theme Sombre</a></li>
-        <li><a class="menu_link" onclick="LoadCSS('/Projetwebl1/ENT/css/color2.css')"><i class="fas fa-sun icone"></i>Theme Clair</a></li>
-        <li><a class="menu_link" href="/Projetwebl1/ENT/settings/parametres/parametres.php"><div class="texteBurger"><i class="fas fa-cogs icone"></i>Paramètres</div></a></li>
-        <li><a class="menu_link" href="/Projetwebl1/ENT/auth/logout.php"><div class="texteBurger"><i class="fas fa-sign-out-alt icone"></i>Se déconnecter</div></a></li>
-      </ul>
-    </nav>
-    <div class="site_pusher">
-      <header>
-        <span class="burger_icon" id="burger_button"><i class="fas fa-bars icone"></i></span>
-        <a href="/Projetwebl1/ENT/"><i class="fas fa-home icone retour"></i></a>
-        <div class="Title">
-          <h1 class="craie">Ecole Millocheau</h1>
-          <h2>GS - CP</h2>
-        </div>
-        <img src="/Projetwebl1/ENT/data/logo_millocheau.png" alt="Logo de l'école millocheau" class="taoki">
-      </header>
-
-      <footer>
+<div class="site_container">
+  <!--menu sandwich (burger) pour pouvoir navigueur entre les différentes pages-->
+  <nav class="burger" id="menu_burger">
+    <ul class="menuListe">
+      <li><a class="menu_cross" id="burger_cross" onclick="refresh();"><i class="fas fa-times icone"></i></a></li>
+      <li><a class="menu_link" href="/Projetwebl1/ENT/publications/cmultimedia/cmedia.php"><div class="texteBurger"><i class="fas fa-book icone"></i>Cahiers multimédias</div></a></li>
+      <li><hr class="hrBurger"></li>
+      <li><a class="menu_link" href="/Projetwebl1/ENT/publications/blog/blog.php"><div class="texteBurger"><i class="fas fa-book-open icone"></i>Blog</div></a></li>
+      <li><hr class="hrBurger"></li>
+      <li><a class="menu_link" href="/Projetwebl1/ENT/publications/files/files.php"><div class="texteBurger"><i class="fas fa-file-archive icone"></i>Archivage de fichier</div></a></li>
+      <li><hr class="hrBurger"></li>
+      <li><a class="menu_link" href="/Projetwebl1/ENT/settings/annuaire/annuaire.php"><div class="texteBurger"><i class="far fa-address-book icone"></i>Annuaire</div></a></li>
+      <li><hr class="hrBurger"></li>
+    </ul>
+    <ul class="menuAutre">
+      <!--n'affiche le bouton vers le menu d'administration que si un admin est connecté-->
+      <?php
+        if ($_SESSION["Admin"] == True) {
+      ?>
+      <li><a class="menu_link" href="/Projetwebl1/ENT/settings/admin/UserCreation.php"><div class="texteBurger"><i class="fas fa-user-cog icone"></i>Administration</div></a></li>
+      <?php
+      }
+      ?>
+      <li><a class="menu_link" onclick="LoadCSS('/Projetwebl1/ENT/css/color1.css')"><i class="fas fa-moon icone"></i>Theme Sombre</a></li>
+      <li><a class="menu_link" onclick="LoadCSS('/Projetwebl1/ENT/css/color2.css')"><i class="fas fa-sun icone"></i>Theme Clair</a></li>
+      <li><a class="menu_link" href="/Projetwebl1/ENT/settings/parametres/parametres.php"><div class="texteBurger"><i class="fas fa-cogs icone"></i>Paramètres</div></a></li>
+      <li><a class="menu_link" href="/Projetwebl1/ENT/auth/logout.php"><div class="texteBurger"><i class="fas fa-sign-out-alt icone"></i>Se déconnecter</div></a></li>
+    </ul>
+  </nav>
+  <div class="site_pusher">
+    <header>
+      <span class="burger_icon" id="burger_button"><i class="fas fa-bars icone"></i></span>
+      <a href="/Projetwebl1/ENT/"><i class="fas fa-home icone retour"></i></a>
+      <div class="Title">
+        <h1 class="craie">Ecole Millocheau</h1>
+        <h2>GS - CP</h2>
+      </div>
+      <img src="/Projetwebl1/ENT/data/logo_millocheau.png" alt="Logo de l'école millocheau" class="taoki">
+    </header>
+  <footer>
     <ul>
         <li><a href="/Projetwebl1/ENT/settings/contact/contact.php" class="link_footer"><i class="fas fa-bug icone"></i>Signaler un problème/Contact</a></li>
         <li><a href="/Projetwebl1/ENT/settings/credits.php" class="link_footer"><i class="fab fa-linux icone"></i>A propos</a></li>
     </ul>
-</footer>
-
+  </footer>
 
 <?php
 }
--- ENT/index.php
+++ ENT/index.php
@@ -1,3 +1,5 @@
+<!--On vérifie si l'utilisateur est connecté, sinon on le redirige vers la page de connexion (voir à la fin)-->
+
 <?php
 session_start();
 
@@ -9,19 +11,26 @@ if ($_SESSION["Connected"] == "True") {
   <head>
     <meta charset="utf-8">
     <title>ENT Millocheau</title>
+    <!--on charge:
+        * un fichier de style pour le syle principal de la page
+        * un fichier de couleur pour le thème choisi par l'utilisateur (à modifier pour que ça se charge en fonction des paramètres de l'utilisateur)
+        * le logo de l'école
+        * des icones utilisées dans les différentes page de l'ENT
+        * le fichier js-->
     <link rel="stylesheet" href="/Projetwebl1/ENT/css/style.css">
     <link rel="stylesheet" href="/Projetwebl1/ENT/css/color1.css">
     <link rel="icon" type="image/png" href="/Projetwebl1/ENT/data/logo_millocheau.png">
     <script src="https://kit.fontawesome.com/f0c5800638.js" crossorigin="anonymous"></script>
     <script src="/Projetwebl1/ENT/js/main.js"></script>
   </head>
-
+    <!--include car code récurrent dans toutes les pages-->
     <?php
       include 'base.php';
     ?>
         
         <div class="Centre_main">
           <div class="ligne">
+            <!--affiche les anniversaires du mois actuel-->
             <div class="Anniversaire">
               <h4><i class="fas fa-birthday-cake icone"></i>Anniversaires<i class="fas fa-birthday-cake icone"></i></h4>
               <ul>
@@ -29,7 +38,7 @@ if ($_SESSION["Connected"] == "True") {
                 <li>Afficher tous ceux du mois actuel</li>
               </ul>
             </div>
-
+            <!--permet d'avoir le moteur de recherche qwant junior sur la page-->
             <div class="qwant">
               <form action="https://www.qwantjunior.com/" method="GET" target="_blank">
                 <a href="https://www.qwantjunior.com/" target="_blank">
@@ -40,6 +49,7 @@ if ($_SESSION["Connected"] == "True") {
               </form>
             </div>
           </div>
+          <!--liens vers le cahier multimédia et le blog (à voir si l'on rajoute un aperçu sur la page d'accueil)-->
           <div class="ligne">
               <div class="raccourciM">
                 <a href="/Projetwebl1/ENT/publications/cmultimedia/cmedia.php" class="shortcut"><i class="fas fa-book racicone"></i><br><p>Cahiers Multimédia</p></a>
