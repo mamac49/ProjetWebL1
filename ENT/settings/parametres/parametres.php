74bd039807e995fea7c2e1509cf24e4857c828c8
Mattéo Langlois <mamac49@free.fr>
2021-05-01 09:11:23 +0200

    upgrade the form in admin page

--- ENT/settings/parametres/parametres.php
+++ ENT/settings/parametres/parametres.php
@@ -87,9 +87,7 @@ if ($_SESSION["Connected"] == true) {
             <input class="texte" type="submit" name="Valider" value="Valider">
           </form>
             <h2 class="texte">Changement de l'image de profil</h2>
-            <?php
-              echo '<img src="<?php AffichageImg(); ?>" alt="Photo de profil" class="PP">';
-            ?>
+            <img src="<?php AffichageImg(); ?>" alt="Photo de profil" class="PP">;
             <span class="texte"><p class="pp"></p><i class="fas fa-folder-open"></i> Charger une image à partir de mon ordinateur</span>
       </div>
   </div>
