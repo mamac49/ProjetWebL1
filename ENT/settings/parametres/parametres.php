3fc4a02c9daee05a3800cae43076de0922855071
Mattéo Langlois <mamac49@free.fr>
2021-05-01 16:49:55 +0200

    try to show the profile picture in settings

--- ENT/settings/parametres/parametres.php
+++ ENT/settings/parametres/parametres.php
@@ -43,7 +43,7 @@ function Affichage() {
       echo 'Erreur d accès à la base de données - FIN';    
       mysqli_close($link);    
   }
-  mysqli_stmt_bind_param($stmt, 1, $_SESSION['Pic']);
+  mysqli_stmt_bind_param($stmt, 0, $_SESSION['Pic']);
   if ($stmt = mysqli_stmt_execute()) {
     $row = mysqli_fetch_array($stmt);
   } else {
