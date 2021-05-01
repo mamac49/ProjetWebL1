1598bb9776bb2e7d744fc9c8997dc6732b65f97b
Mattéo Langlois <mamac49@free.fr>
2021-05-01 17:01:16 +0200

    try to show the profile picture in settings

--- ENT/settings/parametres/parametres.php
+++ ENT/settings/parametres/parametres.php
@@ -94,7 +94,7 @@ if ($_SESSION["Connected"] == true) {
             <input class="texte" type="submit" name="Valider" value="Valider">
           </form>
             <h2 class="texte">Changement de l'image de profil</h2>
-            <img src="src = "<?php echo ' data:image/png;base64,' . base64_encode(Affichage()) . ' '?>" alt="Photo de profil" class="PP">
+            <img src="<?php echo ' data:image/png;base64,' . base64_encode(Affichage()) . ' '?>" alt="Photo de profil" class="PP">
             <span class="texte"><p class="pp"></p><i class="fas fa-folder-open"></i> Charger une image à partir de mon ordinateur</span>
       </div>
   </div>
