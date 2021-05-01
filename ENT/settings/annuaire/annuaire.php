6d2f65c8dd89f24508628126d6be263ebddd9598
Mattéo Langlois <mamac49@free.fr>
2021-05-01 18:19:07 +0200

    try to show the profile picture in settings

--- ENT/settings/annuaire/annuaire.php
+++ ENT/settings/annuaire/annuaire.php
@@ -56,19 +56,19 @@ if ($_SESSION["Connected"] == true) {
       <div id="context_menu" class="context_menu" >
       <ul class="context_menu_list">
         <li class="context_menu_element" id="show_profile">
-          <p class="context_menu_button"><i class="fa fa-address-card" id="context_menu_icons"></i>Profil</p>
+          <p class="context_menu_button texte"><i class="fa fa-address-card" id="context_menu_icons"></i>Profil</p>
         </li>
         <li>
           <hr class="context_menu_hr">
         </li>
         <li class="context_menu_element" id="copy_phone">
-          <p class="context_menu_button"><i class="fa fa-copy" id="context_menu_icons"></i>Copier le numéro de téléphone</p>
+          <p class="context_menu_button texte"><i class="fa fa-copy" id="context_menu_icons"></i>Copier le numéro de téléphone</p>
         </li>
         <li>
           <hr class="context_menu_hr">
         </li>
         <li class="context_menu_element" id="copy_mail">
-          <p class="context_menu_button"><i class="fa fa-envelope-open" id="context_menu_icons"></i>Copier le mail</p>
+          <p class="context_menu_button texte"><i class="fa fa-envelope-open" id="context_menu_icons"></i>Copier le mail</p>
         </li>
       </ul>
       </div>
