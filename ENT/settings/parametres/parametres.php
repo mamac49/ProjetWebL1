2c836538e284d6e925840275ed0eb4b5cee10a03
Mattéo Langlois <mamac49@free.fr>
2021-05-01 12:17:26 +0200

    try to show the profile picture in settings

--- ENT/settings/parametres/parametres.php
+++ ENT/settings/parametres/parametres.php
@@ -1,6 +1,8 @@
 <?php
 session_start();
 
+$ID = $_SESSION['Id'];
+
 function dbConnect() {
   $link = new mysqli('localhost', 'ENT', 'uWBs4M9kIX4PVa2o', 'ENT');
 
@@ -71,7 +73,7 @@ if ($_SESSION["Connected"] == true) {
             <input class="texte" type="submit" name="Valider" value="Valider">
           </form>
             <h2 class="texte">Changement de l'image de profil</h2>
-            <img src="source.php?id=$_SESSION[Id]>" alt="Photo de profil" class="PP">
+            <img src="source.php?id=1>" alt="Photo de profil" class="PP">
             <span class="texte"><p class="pp"></p><i class="fas fa-folder-open"></i> Charger une image à partir de mon ordinateur</span>
       </div>
   </div>
