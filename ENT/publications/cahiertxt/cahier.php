d2792dee953bbbbb739d334a55173dff00501a0b
Mattéo Langlois <mamac49@free.fr>
2021-05-02 10:50:00 +0200

    add agenda

---
+++ ENT/publications/cahiertxt/cahier.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+
+if ($_SESSION["Connected"] == "True") {
+?>
+
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>ENT Millocheau</title>
+    <link rel="stylesheet" href="/Projetwebl1/ENT/css/style.css">
+    <link rel="stylesheet" href="/Projetwebl1/ENT/css/color1.css">
+    <link rel="icon" type="image/png" href="/Projetwebl1/ENT/data/logo_millocheau.png">
+    <script src="https://kit.fontawesome.com/f0c5800638.js" crossorigin="anonymous"></script>
+    <script src="/Projetwebl1/ENT/js/main.js"></script>
+  </head>
+
+  <?php
+      include 'base.php';
+    ?>
+
+
+
+<?php
+} else {
+  header('Location: https://mlanglois.freeboxos.fr//Projetwebl1/ENT/auth/auth.php');
+}
+?>
