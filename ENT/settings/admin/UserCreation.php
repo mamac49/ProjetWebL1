827d042c1f6392b094bc4c8714fc6695d7a0c14c
Mattéo Langlois <mamac49@free.fr>
2021-05-03 15:03:03 +0200

    add a form to delete user

--- ENT/settings/admin/UserCreation.php
+++ ENT/settings/admin/UserCreation.php
@@ -97,7 +97,7 @@ if ($_SESSION["Connected"] == true and $_SESSION["Admin"] == True) {
                 $nb = nombre();
                 for ($x=1; $x<=$nb+2; $x++) {
                 ?>
-                  <option class="texte" value="<?php $x ?>"><?php echo nom($x); ?></option>
+                  <option class="texte" value="<?php $x ?>"><?php echo nom(1); ?></option>
                 <?php } ?>
 
               </select>
