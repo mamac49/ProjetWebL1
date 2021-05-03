7dddaf02c9f396388a62d725c900776b9b71f739
Enzo17101 <78074212+Enzo17101@users.noreply.github.com>
2021-05-03 19:18:07 +0200

    Update test_page.php

--- ENT/settings/tests/test_page.php
+++ ENT/settings/tests/test_page.php
@@ -1,15 +1,28 @@
 
 <?php
-		$x = 1;
-		$link = dbConnect();
-		$sql = "SELECT * FROM `users` WHERE `iduser` = '$x';";
-		$iduser = 0;
-		if ($result = mysqli_query($link, $sql)) {
-			$row = mysqli_fetch_array($result);
-			$iduser = strval($row['iduser']);
-			mysqli_free_result($result);
-			return $iduser;
-		}
+function dbConnect() {
+    // On se connecte à la BD ENT en utilisant le nom d'utilisateur et le mot de passe
+    $link = new mysqli('localhost', 'ENT', 'uWBs4M9kIX4PVa2o', 'ENT');
+
+    // Si il y a une erreur lors de la connexion
+    if (mysqli_connect_errno()) {
+            # Afficher l'erreur et stopper le programme
+            echo 'Erreur d accès à la base' . mysqli_connect_error();
+            exit;
+    }
+    # Retourner la variable link
+    return $link;
+}
+	$x = 1;
+	$link = dbConnect();
+	$sql = "SELECT * FROM `users` WHERE `iduser` = '$x';";
+	$iduser = 0;
+	if ($result = mysqli_query($link, $sql)) {
+		$row = mysqli_fetch_array($result);
+		$iduser = strval($row['iduser']);
+		mysqli_free_result($result);
+		return $iduser;
+	}
 ?>
 
 <!DOCTYPE html>
