26e44724f607537511c4b74cd5b77e9a19e54e99
Mattéo Langlois <mamac49@free.fr>
2021-05-04 13:01:48 +0200

    add a form to create cahier multimedia

--- ENT/publications/cmultimedia/creationC.php
+++ ENT/publications/cmultimedia/creationC.php
@@ -27,8 +27,8 @@ include ("../../base.php");
 <div class="Center">
   <form class="" action="creationC.php" method="post" enctype="multipart/form-data">
     <?php
-    $n = 1
-    $y = 0
+    $n = 1;
+    $y = 0;
     for ($x=1 ; $x=$n ; $x = $x + $y) {
     ?>
     <textarea name="<?php "text-".$n ?>" rows="8" cols="80" resize="none" required></textarea>
