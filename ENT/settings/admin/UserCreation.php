2b97c997cff2eab2e54e620f1d3caec96a9a1eb0
Mattéo Langlois <mamac49@free.fr>
2021-05-04 10:20:46 +0200

    change the Delete methofd

--- ENT/settings/admin/UserCreation.php
+++ ENT/settings/admin/UserCreation.php
@@ -115,7 +115,7 @@ if ($_SESSION["Connected"] == true and $_SESSION["Admin"] == True) {
                 for ($x=1; $x<=$nb+2; $x++) {
                   echo $nb . " " . $x;
                 ?>
-                  <option class="texte" value="<?php echo $x ?>"><?php echo nom($x) . " " . $nb . " " . $x; ?></option>
+                  <option class="texte" value="<?php echo $x ?>"><?php echo nom($x); ?></option>
                 <?php } ?>
               </select>
 
