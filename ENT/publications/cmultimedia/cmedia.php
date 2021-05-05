001d0142737f349bd10baee37fb5bdbf367e8d63
Mattéo Langlois <mamac49@free.fr>
2021-05-05 11:18:57 +0200

    add a title to cahier multimedia

--- ENT/publications/cmultimedia/cmedia.php
+++ ENT/publications/cmultimedia/cmedia.php
@@ -21,6 +21,7 @@ function titre($x) {
 }
 
 function nature($x) {
+  $link = dbConnect();
   $sql = "SELECT `nature` FROM `publications` WHERE `idpublications`='$x'";
   if ($result = mysqli_query($link, $sql)) {
     $row = mysqli_fetch_array($result);
