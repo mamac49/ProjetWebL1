e47c053a5abf77165f4886cc1f50196a2a538922
Enzo17101 <78074212+Enzo17101@users.noreply.github.com>
2021-05-05 18:42:42 +0200

    update test_page.php + bouton.js + styletest.css

---
+++ ENT/settings/tests/bouton.js
@@ -0,0 +1,4 @@
+function bouton() {
+	var p = document.getElementById("bouton");
+	p.className = p.className + " animation_on";
+}
--- ENT/settings/tests/test_page.php
+++ ENT/settings/tests/test_page.php
@@ -1,31 +1,5 @@
 
-<?php
-function dbConnect() {
-    // On se connecte à la BD ENT en utilisant le nom d'utilisateur et le mot de passe
-    $link = new mysqli('localhost', 'ENT', 'uWBs4M9kIX4PVa2o', 'ENT');
-
-    // Si il y a une erreur lors de la connexion
-    if (mysqli_connect_errno()) {
-            # Afficher l'erreur et stopper le programme
-            echo 'Erreur d accès à la base' . mysqli_connect_error();
-            exit;
-    }
-    # Retourner la variable link
-    return $link;
-}
-	function test($x) {
-		$link = dbConnect();
-		$sql = "SELECT * FROM `users` WHERE `iduser` = '$x';";
-		$iduser = 0;
-		if ($result = mysqli_query($link, $sql)) {
-			$row = mysqli_fetch_array($result);
-			$iduser = strval($row['iduser']);
-			mysqli_free_result($result);
-			return $iduser;
-		}
-	}
-?>
-
+<
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
   <head>
@@ -33,8 +7,8 @@ function dbConnect() {
     <title>TEST</title>
   </head>
     <body>
-	<p>
-		<?php echo test(2);?>
+	<p class="animation_off" id="bouton" onclick="bouton()">
+		Bouton
 	</p>
 	</body>
 </html>
