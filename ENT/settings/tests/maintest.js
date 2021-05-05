000ecde0287822faeecd45550eca8356a63df11b
Enzo17101 <78074212+Enzo17101@users.noreply.github.com>
2021-05-05 19:03:25 +0200

    Update maintest.js

--- ENT/settings/tests/maintest.js
+++ ENT/settings/tests/maintest.js
@@ -1,11 +1,12 @@
 function bouton() {
-	var p = document.getElementById("bouton");
-	p.className = "animation_on";
-	buton_state = true;
-}
-
-function bouton() {
-	if(buton_state == true) {
+	if (buton_state = none){
+		buton_state = false;
+	} 
+	if(buton_state == false) {
+		var p = document.getElementById("bouton");
+		p.className = "animation_on";
+		buton_state = true;
+	} else if(buton_state == true) {
 		var p = document.getElementById("bouton");
 		p.className = "animation_off";
 		buton_state = false;
