7d880d3727e1fcf052131df44afbf16e3c6a4e55
Mattéo Langlois <mamac49@free.fr>
2021-05-06 10:37:06 +0200

    add an input to validate password during user creation

--- ENT/js/scroll.js
+++ ENT/js/scroll.js
@@ -4,8 +4,8 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementById("navbar").style.bottom = "0";
+    document.getElementById("navbar").style.bottom = "-50px";
   } else {
-    document.getElementById("navbar").style.bottom = "+50px";
+    document.getElementById("navbar").style.bottom = "0px";
   }
 }
