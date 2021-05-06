07d4d910a845505f714c1a2087d380e429dd2abe
Mattéo Langlois <mamac49@free.fr>
2021-05-06 08:53:44 +0200

    change the fonc in Usercreation.php

--- ENT/settings/admin/UserCreation.php
+++ ENT/settings/admin/UserCreation.php
@@ -109,7 +109,7 @@ if ($_SESSION["Connected"] == true and $_SESSION["Admin"] == True) {
                 $nb = nombre();
                 for ($x=1; $x<=$nb; $x++) {
                 ?>
-                  <option class="texte" value="<?php echo $x ?>"><?php echo info($x)["nom"]; ?></option>
+                  <option class="texte" value="<?php echo $x ?>"><?php echo info($x)["prenom"] . " " . info($x)["nom"]; ?></option>
                 <?php } ?>
               </select>
 
