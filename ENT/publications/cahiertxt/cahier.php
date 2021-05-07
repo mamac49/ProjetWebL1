6e67d483cd883a5baf4e9767a20e19ec64b6e9fa
Mattéo Langlois <mamac49@free.fr>
2021-05-07 16:11:51 +0200

    add a class to the house

--- ENT/publications/cahiertxt/cahier.php
+++ ENT/publications/cahiertxt/cahier.php
@@ -22,6 +22,48 @@ if ($_SESSION["Connected"] == "True") {
       include '../../base.php';
     ?>
 
+    <div class="Center">
+      <div class="semaine">
+        <button class="tablinks" onmouseover="openDay(event, 'Lundi')">Lundi</button>
+        <button class="tablinks" onmouseover="openDay(event, 'Mardi')">Mardi</button>
+        <button class="tablinks" onmouseover="openDay(event, 'Jeudi')">Jeudi</button>
+        <button class="tablinks" onmouseover="openDay(event, 'Vendredi')">Jeudi</button>
+      </div>
+
+      <div id="Lundi" class="tabcontent">
+        <h3>Lundi</h3>
+        <ul>
+          <li>Mathématiques : </li>
+          <li>Français : </li>
+        </ul>
+      </div>
+
+      <div id="Mardi" class="tabcontent">
+        <h3>Mardi</h3>
+        <ul>
+          <li>Mathématiques : </li>
+          <li>Français : </li>
+        </ul>
+      </div>
+
+      <div id="Jeudi" class="tabcontent">
+        <h3>Jeudi</h3>
+        <ul>
+          <li>Mathématiques : </li>
+          <li>Français : </li>
+        </ul>
+      </div>
+
+      <div id="Vendredi" class="tabcontent">
+        <h3>Vendredi</h3>
+        <ul>
+          <li>Mathématiques : </li>
+          <li>Français : </li>
+        </ul>
+      </div>
+
+    </div>
+
 
 
 <?php
