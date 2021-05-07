b7ee0fe1207effebf0e2332a589dd6278d273815
Mattéo Langlois <mamac49@free.fr>
2021-05-07 16:42:02 +0200

    add a class to the house

--- ENT/publications/cahiertxt/cahier.php
+++ ENT/publications/cahiertxt/cahier.php
@@ -35,35 +35,35 @@ if ($_SESSION["Connected"] == "True") {
       <div id="Lundi" class="tabcontent">
         <h3>Lundi</h3>
         <ul>
-          <li><i class="fas fa-square-root-alt icone"></i> Mathématiques : </li>
-          <li><i class="fas fa-flask icone"></i> Science : </li>
-          <li><i class="fas fa-landmark icone"></i> Histoire : </li>
+          <li><i class="fas fa-square-root-alt matiere"></i> Mathématiques : </li>
+          <li><i class="fas fa-flask matiere"></i> Science : </li>
+          <li><i class="fas fa-landmark matiere"></i> Histoire : </li>
         </ul>
       </div>
 
       <div id="Mardi" class="tabcontent">
         <h3>Mardi</h3>
         <ul>
-          <li><i class="fas fa-square-root-alt icone"></i> Mathématiques : </li>
-          <li><i class="fas fa-book icone"></i> Français : </li>
-          <li><i class="fas fa-globe-americas icone"></i> Géographie : </li>
+          <li><i class="fas fa-square-root-alt matiere"></i> Mathématiques : </li>
+          <li><i class="fas fa-book matiere"></i> Français : </li>
+          <li><i class="fas fa-globe-americas matiere"></i> Géographie : </li>
         </ul>
       </div>
 
       <div id="Jeudi" class="tabcontent">
         <h3>Jeudi</h3>
         <ul>
-          <li><i class="fas fa-square-root-alt icone"></i> Mathématiques : </li>
-          <li><i class="fas fa-bicycle"></i> Sport : </li>
-          <li><i class="fas fa-landmark icone"></i> Histoire : </li>
+          <li><i class="fas fa-square-root-alt matiere"></i> Mathématiques : </li>
+          <li><i class="fas fa-bicycle matiere"></i> Sport : </li>
+          <li><i class="fas fa-landmark matiere"></i> Histoire : </li>
         </ul>
       </div>
 
       <div id="Vendredi" class="tabcontent">
         <h3>Vendredi</h3>
         <ul>
-          <li><i class="fas fa-square-root-alt icone"></i> Mathématiques : </li>
-          <li><i class="fas fa-book icone"></i> Français : </li>
+          <li><i class="fas fa-square-root-alt matiere"></i> Mathématiques : </li>
+          <li><i class="fas fa-book matiere"></i> Français : </li>
         </ul>
       </div>
 
