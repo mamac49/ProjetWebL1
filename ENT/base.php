5f96b2a064056091d09ba6a2c4eb0ce80b431600
Enzo17101 <78074212+Enzo17101@users.noreply.github.com>
2021-05-08 19:49:34 +0200

    correction bug css

--- ENT/base.php
+++ ENT/base.php
@@ -1,11 +1,13 @@
+
+
+
 <?php
-
+echo "<script src="\ENT\js\main.js"></script>"
 if ($_SESSION['theme'] == 1) {
-  echo "<script> LoadCSS('/Projetwebl1/ENT/css/color1.css'); </script>";
+  echo "<script>LoadCSS('/Projetwebl1/ENT/css/color1.css'); </script>";
 } else {
-  echo "<script> LoadCSS('/Projetwebl1/ENT/css/color2.css'); </script>";
+  echo "<script>LoadCSS('/Projetwebl1/ENT/css/color2.css'); </script>";
 }
-
 ?>
 
 <head>
