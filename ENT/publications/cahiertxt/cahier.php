16b1e5a8355867bf4b8c881c75209fa97c75eb68
Mattéo Langlois <mamac49@free.fr>
2021-05-08 16:00:15 +0200

    change the loop in cahier.php

--- ENT/publications/cahiertxt/cahier.php
+++ ENT/publications/cahiertxt/cahier.php
@@ -91,9 +91,9 @@ if ($_SESSION["Connected"] == "True") {
         <div id="<?php echo $jour; ?>" class="tabcontent">
           <h3><?php echo $jour; ?></h3>
           <ul>
-          <?php if ($_SESSION["Classe"] == "GS" OR $_SESSION["Admin"] == True) { ?>
-            <li>Salut</li>
-            <?php for ($i=1; $i <= NbDevoir($jour, "GS"); $i++) {
+          <?php if ($_SESSION["Classe"] == "GS" OR $_SESSION["Admin"] == True) {
+            echo AfficherDevoir($jour, "GS");
+            for ($i=1; $i <= NbDevoir($jour, "GS"); $i++) {
               $info = AfficherDevoir($jour, "GS");
               $matiere = $info['matiere'];
               $consigne = $info['consigne']; ?>
