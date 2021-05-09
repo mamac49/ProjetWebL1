ae21746a5db18febdf87fa10194e0f1988626375
Mattéo Langlois <mamac49@free.fr>
2021-05-09 22:52:22 +0200

    update cahier.php

--- ENT/publications/cahiertxt/cahier.php
+++ ENT/publications/cahiertxt/cahier.php
@@ -131,24 +131,25 @@ if ($_SESSION["Connected"] == "True") {
       <?php foreach ($semaine as $jour) {?>
         <div id="<?php echo $jour; ?>" class="tabcontent">
           <h3><?php echo $jour; ?></h3>
-            <?php foreach ($matiere as $x) { ?>
             <ul>
+              <?php foreach ($matiere as $x) { ?>
               <?php for ($i=0; $i < NbPubJour($jour, "GS"); $i++) {
                    $info = AfficherDevoir($jour, "GS");
                    $matiereP = $info['matiere'];
                    $consigne = $info['consigne']; ?>
                   <li class="texte"><?php echo "<i class='$matiere[$matiereP] matiere'></i>" . $matiereP . " : " . $consigne; ?></li>
-              <?php } ?>
+              <?php }} ?>
 
+              <?php foreach ($matiere as $x) { ?>
               <?php for ($i=0; $i < NbPubJour($jour, "CP"); $i++) {
                    $info = AfficherDevoir($jour, "CP");
                    $matiereP = $info['matiere'];
                    $consigne = $info['consigne']; ?>
                   <li class="texte"><?php echo "<i class='$matiere[$matiereP] matiere'></i>" . $matiereP . " : " . $consigne; ?></li>
-              <?php } ?>
+              <?php }} ?>
             </ul>
         </div>
-      <?php }} ?>
+      <?php } ?>
 
 
 
