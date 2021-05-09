023b75860cb1e3df126726f7b033d581919baec5
Mattéo Langlois <mamac49@free.fr>
2021-05-09 09:30:56 +0200

    update cahier.php

--- ENT/publications/cahiertxt/cahier.php
+++ ENT/publications/cahiertxt/cahier.php
@@ -17,10 +17,10 @@ function DevoirID() {
   return $cahiersIds;
 }
 
-function NbPubJour($jour) {
+function NbPubJour($jour, $classe) {
   $link = dbConnect();
 
-  $sql = "SELECT `idtxt` FROM `cahiertxt` WHERE `jour`='$jour'";
+  $sql = "SELECT `idtxt` FROM `cahiertxt` WHERE (`jour`='$jour' AND `classe`='$classe')";
   $result = mysqli_query($link, $sql);
   $nb = array();
   if ($result) {
@@ -122,27 +122,28 @@ if ($_SESSION["Connected"] == "True") {
         <button class="tablinks" onmouseover="openDay(event, 'Vendredi')">Vendredi</button>
       </div>
 
-      <?php foreach ($semaine as $jour) {
-        for ($i=1 ; $i <= NbPubJour($jour) ; $i++) { ?>
+      <?php foreach ($semaine as $jour) {?>
               <div id="<?php echo $jour; ?>" class="tabcontent">
                 <h3><?php echo $jour; var_dump(NbPubJour($jour)); ?></h3>
                 <ul>
-                  <?php if ($_SESSION["Classe"] == "GS" OR $_SESSION["Admin"] == True) {
+                  <?php for ($i=1 ; $i <= NbPubJour($jour, "GS") ; $i++) {
+                        if ($_SESSION["Classe"] == "GS" OR $_SESSION["Admin"] == True) {
                             $info = AfficherDevoir($jour, "GS");
                             $matiereP = $info['matiere'];
                             $consigne = $info['consigne']; ?>
                             <li class="texte"><?php echo "<i class='$matiere[$matiereP] matiere'></i>" . $matiereP . " : " . $consigne; ?></li>
-                    <?php } ?>
+                    <?php }} ?>
 
-                    <?php if ($_SESSION["Classe"] == "CP" OR $_SESSION["Admin"] == True) {
+                    <?php for ($i=1 ; $i <= NbPubJour($jour, "CP") ; $i++) {
+                          if ($_SESSION["Classe"] == "CP" OR $_SESSION["Admin"] == True) {
                               $info = AfficherDevoir($jour, "CP");
                               $matiereP = $info['matiere'];
                               $consigne = $info['consigne']; ?>
                               <li class="texte"><?php echo "<i class='$matiere[$matiereP] matiere'></i>" . $matiereP . " : " . $consigne; ?></li>
-                    <?php } ?>
+                    <?php }} ?>
           </ul>
         </div>
-      <?php }} ?>
+      <?php } ?>
 
 
 
