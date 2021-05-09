5dca5c08d3e5a40a368d9056a59ff464262436f2
Epikminey <>
2021-05-09 22:53:23 +0200

    Update blog.php

--- ENT/publications/blog/blog.php
+++ ENT/publications/blog/blog.php
@@ -75,7 +75,7 @@ if ($_SESSION["Connected"] == true) {
                   $auteur=auteur($i[0]);
                   $date=jour($i[0]);
               ?>
-              <li class="sujets"><a href="AffichageB.php?id=2"><i class="fas fa-robot icone"></i> <?php echo $titre; ?>
+              <li class="sujets"><a href="AffichageB.php?id=<?php print $i ?>"><i class="fas fa-robot icone"></i> <?php echo $titre; ?>
               </a> <span class="texte">Edité par <?php echo $auteur; ?> le <?php echo $date; ?></span></li>
               <li class="espaces"><hr></li>
               <?php
