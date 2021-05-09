4edb703a31fd4dae2502c58f3f235098889ab4f7
Mattéo Langlois <mamac49@free.fr>
2021-05-10 00:02:36 +0200

    update cahier.php

--- ENT/publications/cahiertxt/cahier.php
+++ ENT/publications/cahiertxt/cahier.php
@@ -135,13 +135,13 @@ if ($_SESSION["Connected"] == "True") {
           <h3><?php echo $jour; ?></h3>
             <ul>
               <?php
-                 for ($i=0; $i < NbPubJour($jour, "GS"); $i++) {
+                 /*for ($i=0; $i < NbPubJour($jour, "GS"); $i++) {*/
                   foreach ($ListMatiere as $x) {
                     if (gettype(AfficherDevoir($jour, "GS", $x)) != "NULL") {
                        $info = AfficherDevoir($jour, "GS", $x);
                        $consigne = $info['consigne']; ?>
                       <li class="texte"><?php echo "<i class='$matiere[$x] matiere'></i>" . $x . " : " . $consigne; ?></li>
-              <?php }}} ?>
+              <?php }} ?>
 
               <?php
                 for ($i=0; $i < NbPubJour($jour, "CP"); $i++) {
