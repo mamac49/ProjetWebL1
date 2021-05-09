d5bfbea18d25d767ebedc4668fe25bc37360e306
Mattéo Langlois <mamac49@free.fr>
2021-05-09 23:16:36 +0200

    update cahier.php

--- ENT/publications/cahiertxt/cahier.php
+++ ENT/publications/cahiertxt/cahier.php
@@ -136,8 +136,7 @@ if ($_SESSION["Connected"] == "True") {
             <ul>
               <?php foreach ($ListMatiere as $x) { ?>
               <?php for ($i=0; $i < NbPubJour($jour, "GS"); $i++) {
-                var_dump(AfficherDevoir($jour, "GS", $x));
-                if (count(AfficherDevoir($jour, "GS", $x))>0) {
+                if (AfficherDevoir($jour, "GS", $x) == null) {
                    $info = AfficherDevoir($jour, "GS", $x);
                    $matiereP = $info['matiere'];
                    $consigne = $info['consigne']; ?>
