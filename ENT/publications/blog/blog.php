a0b2bd9d65613825d26fa816eaa5dbf590f68478
Epikminey <>
2021-05-09 22:15:32 +0200

    Update blog.php

--- ENT/publications/blog/blog.php
+++ ENT/publications/blog/blog.php
@@ -5,7 +5,7 @@ include ("../../fonc.php");
 
 function auteur($x) {
   $link = dbConnect();
-  $sql = "SELECT `prenom`, `nom` FROM `users` WHERE iduser=(SELECT `iduser` FROM Publications WHERE `nature`=1 AND `idpublications`='$x')";
+  $sql = "SELECT `prenom`, `nom` FROM `users` WHERE iduser=(SELECT `iduser` FROM `Publications` WHERE `nature`=1 AND `idpublications`='$x')";
   if ($result = mysqli_query($link, $sql)) {
     $row = mysqli_fetch_array($result);
     return $row[0]." ".$row[1];
