a6c17a5a57638c931b645fc6e6d5c145277b5341
Enzo17101 <78074212+Enzo17101@users.noreply.github.com>
2021-05-10 12:00:48 +0200

    Update main.js

--- ENT/js/main.js
+++ ENT/js/main.js
@@ -41,17 +41,19 @@ function getNbLines() {
 
 /*Fonction d'ajout de texte ou d'image*/
 
-
-function addLine(lineType) {
   /*déclaration des variables :
   -textArea : template de la saisie de texte
   -inputImage : template du dépot d'image
   -line : ligne de la zone
   -publicationCahierMultimedia : liste où sont affichés les zones de saisie*/
-  var textArea = '<textarea name="line_0" class="texteCahierMulimedia" id="text_cahier_multimedia"  title="texte" rows="8" cols="80" resize="none" create="false" required=""></textarea>';
-  var inputImage = '<input name="line_0" class="imageCahierMulimedia" type="image" id="image_cahier_multimedia" accept="image/*" required>';
-  var line = 0;
-  var publicationCahierMultimedia = document.getElementById("publications_cahier_multimedia");
+textArea = '<textarea name="line_0" class="texteCahierMulimedia" id="text_cahier_multimedia"  title="texte" rows="8" cols="80" resize="none" create="false" required=""></textarea>';
+inputImage = '<input name="line_0" class="imageCahierMulimedia" type="image" id="image_cahier_multimedia" accept="image/*" required>';
+line = 0;
+
+function addLine(lineType) {
+  /*-publicationCahierMultimedia : liste où sont affichés les zones de saisie*/
+  publicationCahierMultimedia = document.getElementById("publications_cahier_multimedia");
+
   /*vérifie si l'on a commencé par un texte*/
   if (publicationCahierMultimedia.innerHTML.indexOf(textArea) == -1 && lineType == 'text') {
     /* DEBUT DU MONDE !!!*/
