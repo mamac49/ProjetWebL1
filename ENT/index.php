4830c29ff86366c854b8d431b2d76e0f51374acc
Teclock <liam.kern@orange.fr>
2021-05-10 15:25:26 +0100

    Update index.php

--- ENT/index.php
+++ ENT/index.php
@@ -8,7 +8,7 @@ include 'fonc.php';
 function annivs() {
     $link = dbConnect();
     $moisactu = date("m");
-    $anniv = "SELECT * FROM `users` WHERE `date_n` LIKE '%/'.$moisactu%.'/%'";
+    $anniv = "SELECT * FROM `users` WHERE `date_n` LIKE '%/$moisactu/%'";
 
     if ($result = mysqli_query($link, $anniv)) {
       $row = mysqli_fetch_array($result);
