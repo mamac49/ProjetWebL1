fe3dc0fefcb3db81f733326aa81a080f56e4af49
Mattéo Langlois <mamac49@free.fr>
2021-05-10 15:34:23 +0200

    update index.php

--- ENT/index.php
+++ ENT/index.php
@@ -15,7 +15,7 @@ function annivs() {
     if ($result = mysqli_query($link, $anniv)) {
       $row = mysqli_fetch_array($result);
       if (is_null($row)) {
-          $retour = $row;
+          $retour = $anniv;
       } else {
         mysqli_free_result($result);
         $retour = $row;
