f20ccc69095b5fa0962ec4c84a36aeaa2d6268e8
Mattéo Langlois <mamac49@free.fr>
2021-05-10 16:34:04 +0200

    update UserCreation.ph

--- ENT/settings/admin/UserCreation.php
+++ ENT/settings/admin/UserCreation.php
@@ -49,7 +49,7 @@ if ( isset($_POST['valider'])) {
       $mail = securisation($_POST['mail']);
       $classe = securisation($_POST['classe']);
       $date = securisation($_POST['datenaissance']);
-      $pp = file_get_contents("../../data/PP.png", FALSE);
+      $pp = file_get_contents("../../data/PP.png", True);
       if (isset($_POST['admin'])) {
         $admin = securisation($_POST['admin']);
       } else {
