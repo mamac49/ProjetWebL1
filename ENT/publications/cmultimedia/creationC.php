091bb847145d50818065e5d788829e80d083e24a
Enzo17101 <78074212+Enzo17101@users.noreply.github.com>
2021-05-10 22:22:07 +0200

    Update creationC.php

--- ENT/publications/cmultimedia/creationC.php
+++ ENT/publications/cmultimedia/creationC.php
@@ -45,10 +45,12 @@ if ($_SESSION["Connected"] == true) {
   <form class="" action="creationC.php" method="POST" enctype="multipart/form-data">
     <input type="text" name="titre" placeholder="Titre du cahier" >
     <ul class="publicationsCahierMultimedia" id="publications_cahier_multimedia">
+      <iframe name="line_0" width="65%" height="30%" src="https://youtu.be/clmOwRIVoaE" title="video" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </ul>
     <div class="boutonsCahierMultimedia">
-      <button class="boutonAjouterTexte bouton" id="add_text" onclick="addText()"><span>Ajouter un texte </span></button>
-      <button class="boutonAjouterImage bouton" id="add_image" onclick="addImage()"><span>Ajouter une image </span></button>
+      <button class="boutonAjouterTexte bouton" id="add_text" onclick="addText()"><span>Ajouter un texte</span></button>
+      <button class="boutonAjouterImage bouton" id="add_image" onclick="addImage()"><span>Ajouter une image</span></button>
+      <button class="boutonAjouterVideo bouton" id="add_video" onclick="addVideo()"><span>Ajouter une vidéo</span></button>
       <!--<button name="create" class="bouton" onclick="addVideo()">Ajouter une case</button>-->
       <input type="submit" name="Valider" class="boutonValider bouton" value="Valider de cahier multimédia">
     </div>
