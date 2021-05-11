2981d96cf2fd14459073154c2aa9c3cb38daa52a
Mattéo Langlois <mamac49@free.fr>
2021-05-11 15:01:42 +0200

    update CreationC.php

--- ENT/publications/cmultimedia/AffichageC.php
+++ ENT/publications/cmultimedia/AffichageC.php
@@ -69,9 +69,7 @@ if ($_SESSION["Connected"] == true) {
             if (substr_count($line, "http") == 1) {
               if (substr_count($line, "https://www.deezer.com/") == 1) {
                 $track = str_replace("https://www.deezer.com/us/track/", "", $line);
-                echo "<iframe title='deezer-widget'
-                src='https://widget.deezer.com/widget/dark/track/'. $track .'?tracklist=false' width='300px' height='150px' frameborder='0' allowtransparency='true'
-                 allow='encrypted-media; clipboard-write'></iframe>";
+                echo "<iframe title='deezer-widget' src='https://widget.deezer.com/widget/dark/track/'. $track .'?tracklist=false' width='300px' height='150px' frameborder='0' allowtransparency='true' allow='encrypted-media; clipboard-write'></iframe>";
               } else {
                 echo "<a href=". $line .">". $line ."</a><br>";
               }
