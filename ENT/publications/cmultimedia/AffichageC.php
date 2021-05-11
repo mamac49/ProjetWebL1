c4bc129a29b5af1876bcb9501357e06a37415ea3
Mattéo Langlois <mamac49@free.fr>
2021-05-11 16:29:36 +0200

    update AffichageC.php

--- ENT/publications/cmultimedia/AffichageC.php
+++ ENT/publications/cmultimedia/AffichageC.php
@@ -32,7 +32,7 @@ function AffichageCahier($ID) {
   $images = array();
   if ($result) {
     while($row = $result->fetch_array(MYSQLI_BOTH)) {
-      $images[$row["position"]] = $row["idimage"];
+      $images[$row["position"]] = "ImageContenu" . $row["idimage"];
     }
   }
   $liste = array();
