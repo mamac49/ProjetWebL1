d495e53896e1c066df2ec7a62526caa8dce4bf96
Mattéo Langlois <mamac49@free.fr>
2021-05-11 16:41:58 +0200

    update AffichageC.php

--- ENT/publications/cmultimedia/creationC.php
+++ ENT/publications/cmultimedia/creationC.php
@@ -22,6 +22,7 @@ function Create($titre, $contenu) {
         $sqlp = "INSERT INTO `liens` (`idlien`, `data`, `position`, `idpublications`) VALUES ('$nb', '$element', '$pos', '$sqlID')";
       } elseif (substr_count($element, "ImageContenu") == 1) {
         $nb = array_key_last(nombreTxt("image"))+1;
+        var_dump(array_key_last(nombreTxt("image")));
         $line = str_replace("ImageContenu", "", $line);
         $img = mysqli_real_escape_string($link, $element);
         $sqlp = "INSERT INTO `image` (`idimage`, `data`, `position`, `idpublications`) VALUES ('$nb', '$img', '$pos', '$sqlID')";
