ba14b8b571b6c3cae265e6f504079b9918b054fa
Mattéo Langlois <mamac49@free.fr>
2021-05-11 15:39:34 +0200

    update CreationC.php

--- ENT/publications/cmultimedia/AffichageC.php
+++ ENT/publications/cmultimedia/AffichageC.php
@@ -106,9 +106,10 @@ if ($_SESSION["Connected"] == true) {
               } else {
                 echo "<a href=". $line .">". $line ."</a><br>";
                 }
-            } elseif (substr_count($element, "/tmp/php") { ?>
+            } elseif (substr_count($element, "/tmp/php") {
+                ?>
                 <img src="<?php echo ' data:image/png;base64,' . base64_encode(AffichageCM($element)) . ' '?>" alt="Image" class="ImgCM">
-            <?php
+                <?php
             } else {
               echo "<pre>". $line ."</pre>";
             }} ?>
