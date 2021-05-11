a3045f006eb927bcbc5615c0c1c69f4141de9d31
Mattéo Langlois <mamac49@free.fr>
2021-05-11 08:44:08 +0200

    add a function to create cahier

--- ENT/publications/cmultimedia/creationC.php
+++ ENT/publications/cmultimedia/creationC.php
@@ -17,8 +17,6 @@ if (isset($_POST['Valider'])) {
   while (isset($_POST['line_' . $nb])) {
     $temp = $_POST['line_' . $nb];
     $contenu[] = $temp;
-    var_dump($temp);
-    var_dump($contenu);
     $nb++;
   }
   Create($titre, $contenu);
