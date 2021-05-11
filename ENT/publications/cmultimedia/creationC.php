c1d3c329b62ca97f3d0d9ea002826c036afde7f8
Mattéo Langlois <mamac49@free.fr>
2021-05-11 11:23:10 +0200

    update creationC.php

--- ENT/publications/cmultimedia/creationC.php
+++ ENT/publications/cmultimedia/creationC.php
@@ -19,10 +19,10 @@ function Create($titre, $contenu) {
     foreach ($contenu as $element) {
       if (filter_var($element, FILTER_VALIDATE_URL)) {
         $nb = nombreTxt("image");
-        $sqlp = "INSERT INTO `liens` (`data`, `position`, `idpublications`) VALUES ('$nb', '$element', '$pos', '$sqlID')";
+        $sqlp = "INSERT INTO `liens` (`idlien`, `data`, `position`, `idpublications`) VALUES ('$nb', '$element', '$pos', '$sqlID')";
       } else {
         $nb = nombreTxt("texte");
-        $sqlp = "INSERT INTO `texte` (`data`, `position`, `idpublications`) VALUES ('$nb', '$element', '$pos', '$sqlID')";
+        $sqlp = "INSERT INTO `texte` (`idtexte`, `data`, `position`, `idpublications`) VALUES ('$nb', '$element', '$pos', '$sqlID')";
       }
       $pos++;
       if (mysqli_query($link, $sql)) {
