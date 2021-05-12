523bc03c7c6fbdaccf7a84f349e6815a1f7a0173
Teclock <liam.kern@orange.fr>
2021-05-12 13:09:34 +0100

    columns

--- ENT/index.php
+++ ENT/index.php
@@ -46,7 +46,7 @@ if ($_SESSION["Connected"] == "True") {
     ?>
 
     <div class="Centre_main">
-      <div class="colonne">
+      <div class="has_cols">
         <!--affiche les anniversaires du mois actuel-->
         <div class="Anniversaire">
           <h4 class="texteB"><i class="fas fa-birthday-cake icone"></i>Anniversaires<i class="fas fa-birthday-cake icone"></i></h4>
