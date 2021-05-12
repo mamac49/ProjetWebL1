e18c240ceec03ca7eb2c32c2d61bb5c5b744248a
Enzo17101 <78074212+Enzo17101@users.noreply.github.com>
2021-05-12 12:14:37 +0200

    Update annuaire.php

--- ENT/settings/annuaire/annuaire.php
+++ ENT/settings/annuaire/annuaire.php
@@ -31,34 +31,37 @@ if ($_SESSION["Connected"] == true) {
   <body onload="move_menu_sandwich(); detect_click();">
 
     <div class="site_container">
-          <div class="Center_adap">
-            <!-- les contacts seront listé avec en premier le professeur
-              et ensuite les élèves de la classe dans l'ordre alphabétique des noms/-->
-            <h2 class="titre_liste texte">Liste des contacts</h2>
-            <ul class="contact_list">
-            <?php
-              foreach (nombre() as $x) {
-                $contact_name = info($x[0])['prenom'] . " " . info($x[0])['nom'];
-                $contact_id = "contact_" . $x[0];
-                $mail = info($x[0])["mail"];
-            ?>
-              <li class="contact">
-                <div class="debut"><a class="<?php echo $contact_id;?> texte" id="contact"><span><img src="<?php echo ' data:image/png;base64,' . base64_encode(Affichage($mail)) . ' '?>" alt="Photo de profil" class="PPannuaire">
-                  <?php echo $contact_name;?></span></a></div>
-                <div class="fin">
-                  <div class="dropdown"><button onclick="down(<?php print $x[0];?>)" class="dropbtn">Information</button></div>
-                  <div id="myDropdown-<?php echo $x[0];?>" class="dropdown-content">
-                    <button class="btn-info" onclick="copy('<?php print 'Mail-' . $x[0];?>')"><i class="fas fa-envelope"></i><p id="ToCopyMail-<?php echo $x[0];?>"><?php echo $mail; ?></p></button>
-                    <button class="btn-info" onclick="copy('<?php print 'Contact-' . $x[0];?>')"><i class="fas fa-user-tag"></i><p id="ToCopyContact-<?php echo $x[0];?>"><?php echo $contact_name; ?></p></button>
-                  </div>
-
-                </div>
-              </li>
-              <li><hr class="hrcontact"></li>
-            <?php
-                }
-            ?>
-            </ul>
+      <div class="Center_adap">
+        <!-- les contacts seront listé avec en premier le professeur
+          et ensuite les élèves de la classe dans l'ordre alphabétique des noms/-->
+        <h2 class="titre_liste texte">Liste des contacts</h2>
+        <ul class="contact_list">
+        <?php
+          foreach (nombre() as $x) {
+            $contact_name = info($x[0])['prenom'] . " " . info($x[0])['nom'];
+            $contact_id = "contact_" . $x[0];
+            $mail = info($x[0])["mail"];
+        ?>
+          <li class="contact">
+            <div class="debut"><a class="<?php echo $contact_id;?> texte" id="contact"><span><img src="<?php echo ' data:image/png;base64,' . base64_encode(Affichage($mail)) . ' '?>" alt="Photo de profil" class="PPannuaire">
+              <?php echo $contact_name;?></span></a>
+            </div>
+            <div class="fin">
+            <div class="dropdown">
+              <button onclick="down(<?php print $x[0];?>)" class="dropbtn">Information</button>
+            </div>
+            <div id="myDropdown-<?php echo $x[0];?>" class="dropdown-content">
+              <button class="btn-info" onclick="copy('<?php print 'Mail-' . $x[0];?>')"><i class="fas fa-envelope"></i><p id="ToCopyMail-<?php echo $x[0];?>"><?php echo $mail; ?></p></button>
+              <button class="btn-info" onclick="copy('<?php print 'Contact-' . $x[0];?>')"><i class="fas fa-user-tag"></i><p id="ToCopyContact-<?php echo $x[0];?>"><?php echo $contact_name; ?></p></button>
+            </div>
+          </li>
+          <li>
+            <hr class="hrcontact">
+          </li>
+        <?php
+          }
+        ?>
+        </ul>
       </div>
     </div>
   </body>
