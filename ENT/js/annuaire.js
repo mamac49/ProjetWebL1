4f8cf87d24d93f0f687773c094a904d21876ad44
Enzo17101 <78074212+Enzo17101@users.noreply.github.com>
2021-05-12 13:03:09 +0200

    Update annuaire.js

--- ENT/js/annuaire.js
+++ ENT/js/annuaire.js
@@ -1,10 +1,12 @@
+
+
 /* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
+toggle between hiding and showing the dropdown content 
 function down(x) {
   document.getElementById("myDropdown-".concat(x.toString())).classList.toggle("show");
 }
 
-// Close the dropdown menu if the user clicks outside of it
+ Close the dropdown menu if the user clicks outside of it
 window.onclick = function(event) {
   if (!event.target.matches('.dropbtn')) {
     var dropdowns = document.getElementsByClassName("dropdown-content");
@@ -19,13 +21,13 @@ window.onclick = function(event) {
 }
 
 function copy(x) {
-  /* Get the text field */
+   Get the text field 
   var copyText = document.getElementById("ToCopy".concat(x.toString()));
   console.log(copyText);
 
-  /* Copy the text inside the text field */
+   Copy the text inside the text field 
   document.execCommand("copy");
 
-  /* Alert the copied text */
+   Alert the copied text 
   alert("Copied the text: " + copyText.textContent);
-}
+}*/
