b65618474e9bd47f6e0149123a9964a252343e05
Mattéo Langlois <mamac49@free.fr>
2021-05-12 13:18:30 +0200

    update creationB.php

--- ENT/publications/blog/creationB.php
+++ ENT/publications/blog/creationB.php
@@ -65,7 +65,7 @@ if ($_SESSION["Connected"] == true) {
     <link rel="stylesheet" media="all and (min-width: 1024px)" href="/Projetwebl1/ENT/css/style.css">
     <link rel="stylesheet" media="all and (min-width: 1024px)" href="/Projetwebl1/ENT/css/styleLittle.css">
     <link rel="stylesheet" media="all and (max-width: 600px)" href="/Projetwebl1/ENT/css/stylePhone.css">
-    <link rel="icon" type="image/png" href="/Projetwebl1/ENT/data/Taoki.png">
+    <link rel="icon" type="image/png" href="/Projetwebl1/ENT/data/logo_millocheau.png">
     <script src="https://kit.fontawesome.com/f0c5800638.js" crossorigin="anonymous"></script>
     <script src="/Projetwebl1/ENT/js/main.js"></script>
     <script src="/Projetwebl1/ENT/js/scroll.js"></script>
