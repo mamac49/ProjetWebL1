43192b02a6eb1e7cd72edd39d6239711bd7f2ddf
Mattéo Langlois <mamac49@free.fr>
2021-05-12 09:56:43 +0200

    update annuaire.hs

--- ENT/publications/files/files.php
+++ ENT/publications/files/files.php
@@ -12,7 +12,8 @@ if ($_SESSION["Connected"] == true) {
     <meta charset="utf-8">
     <title>ENT Millocheau</title>
     <link rel="stylesheet" href="/Projetwebl1/ENT/css/color1.css">
-    <link rel="stylesheet" href="/Projetwebl1/ENT/css/style.css">
+    <link rel="stylesheet" media="all and (min-width: 1024px)" href="/Projetwebl1/ENT/css/style.css">
+    <link rel="stylesheet" media="all and (max-width: 1024px)" href="/Projetwebl1/ENT/css/stylePhone.css">
     <link rel="icon" type="image/png" href="/Projetwebl1/ENT/data/logo_millocheau.png">
     <script src="https://kit.fontawesome.com/f0c5800638.js" crossorigin="anonymous"></script>
     <script src="/Projetwebl1/ENT/js/main.js"></script>
--- ENT/settings/admin/UserCreation.php
+++ ENT/settings/admin/UserCreation.php
@@ -54,8 +54,8 @@ if ($_SESSION["Connected"] == true and $_SESSION["Admin"] == True) {
   <head>
     <meta charset="utf-8">
     <title>Page d'administration</title>
-    <link rel="stylesheet" href="/Projetwebl1/ENT/css/style.css">
-    <link rel="stylesheet" href="styleAd.css">
+    <link rel="stylesheet" media="all and (min-width: 1024px)" href="/Projetwebl1/ENT/css/style.css">
+    <link rel="stylesheet" media="all and (max-width: 1024px)" href="/Projetwebl1/ENT/css/stylePhone.css">
     <link rel="stylesheet" href="/Projetwebl1/ENT/css/color1.css">
     <link rel="icon" type="image/png" href="/Projetwebl1/ENT/data/logo_millocheau.png">
     <script src="https://kit.fontawesome.com/f0c5800638.js" crossorigin="anonymous"></script>
