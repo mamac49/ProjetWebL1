a6f7fafc3d487ec778c19ed3d9d41a1f33be6025
Mattéo Langlois <mamac49@free.fr>
2021-05-12 09:05:36 +0200

    update annuaire.hs

--- ENT/js/annuaire.js
+++ ENT/js/annuaire.js
@@ -22,12 +22,6 @@ function copy(x) {
   /* Get the text field */
   var copyText = document.getElementById("ToCopy".concat(x.toString()));
   console.log(copyText);
-  console.log("ToCopy".concat(x.toString()));
-  console.log(Object.values(copyText));
-
-  /* Select the text field */
-  copyText.select();
-  copyText.setSelectionRange(0, 99999); /* For mobile devices */
 
   /* Copy the text inside the text field */
   document.execCommand("copy");
