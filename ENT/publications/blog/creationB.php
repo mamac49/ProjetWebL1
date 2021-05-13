c7e469360ea3c524f271311cc9c2da8fcaa2c010
Mattéo Langlois <mamac49@free.fr>
2021-05-13 16:36:20 +0200

    update cmedia.php

--- ENT/publications/blog/creationB.php
+++ ENT/publications/blog/creationB.php
@@ -44,10 +44,6 @@ function Create($titre, $matiere, $contenu) {
 
 if (isset($_POST['Valider'])) {
   $titre = securisation($_POST['titre']);
-  if ($titre = "NeverGonnaGiveYouUp") {
-    header('Location: https://www.youtube.com/watch?v=dQw4w9WgXcQ');
-    die();
-  } else {
   $matiere = securisation($_POST['matiere']);
   $contenu = array();
   $nb = 1;
@@ -57,7 +53,7 @@ if (isset($_POST['Valider'])) {
     $nb++;
   }
   Create($titre, $matiere, $contenu);
-}}
+}
 
 if ($_SESSION["Connected"] == true) {
 ?>
