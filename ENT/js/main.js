a9ae6951b28f279af84f5506e29d7340589cbf06
Enzo17101 <78074212+Enzo17101@users.noreply.github.com>
2021-05-14 13:17:15 +0200

    test

--- ENT/js/main.js
+++ ENT/js/main.js
@@ -132,18 +132,7 @@ function addLine(lineType) {
     }
   }
 
-/*Zone de saisie adaptative
 
-function textAdapt(){
-  var textadapt = document.getElementById("text_cahier_multimedia");
-  var textAreaHeight = textadapt.style.height;
-  Si l'on appuie sur entrée on adpate la taille de la zone de texte
-  textArea.addEventListener("keyup", function(event) {
-    if (event.keycode === 13) {
-      textAreaHeight = textAreaHeight.replace(textAreaHeight[0] + "rem", textAreaHeight = (textAreaHeight[0] + 1) + "rem")
-    }
-  });
-}*/
 
 function ReadMore(x) {
   var moreText = document.getElementById("more-" + x);
